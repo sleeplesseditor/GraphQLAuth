bd993991a6bd8227ea35c953dd8bbc2b300dc6f3
sleeplesseditor <josh@bryson.me.uk>
2018-05-17 12:54:35 +0100

    RequireAuth corrected and README updated

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # GraphQLAuth
 
-An example of authorisation built using React and GraphQL. The app uses a starter boilerplate from Stephen Grider's Udemy [course](https://github.com/StephenGrider/auth-graphql-starter).
+An example of authorisation built using React and GraphQL. The app uses a starter boilerplate from Stephen Grider's Udemy [course](https://github.com/StephenGrider/auth-graphql-starter). PassportJS is used for authentication in a coupled approach.
+
+To get this up and running, follow these steps:
+
+1) cd GraphQLAuth
+2) npm install
+3) Place your own mongoLab URI in server.js
+4) npm run dev
+
+
+![screen shot 2018-05-17 at 12 50 07](https://user-images.githubusercontent.com/25869284/40175784-31c3fe06-59d1-11e8-95d4-ee5e6923dda9.png)
+
+App landing screen
+
+
+![screen shot 2018-05-17 at 12 50 29](https://user-images.githubusercontent.com/25869284/40175798-3c99f13c-59d1-11e8-97b3-b586d068c0a6.png)
+
+App Sign Up Screen
+
+
+![screen shot 2018-05-17 at 12 50 38](https://user-images.githubusercontent.com/25869284/40175806-456492a4-59d1-11e8-9c60-b74c997b2d29.png)
+
+App Log In Screen
+
+
+![screen shot 2018-05-17 at 12 50 58](https://user-images.githubusercontent.com/25869284/40175817-4cf1193e-59d1-11e8-8456-e59875e3a35d.png)
+
+App Dashboard Screen
--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
-import AuthForm from './AuthForm';
-import mutation from '../mutations/Login';
 import { graphql } from 'react-apollo';
-import query from '../queries/CurrentUser';
+import currentUserQuery from '../queries/CurrentUser';
 import { hashHistory } from 'react-router';
 
-class LoginForm extends Component {
-  constructor(props) {
-    super(props);
+export default (WrappedComponent) => {
+  class RequireAuth extends Component {
+    componentWillUpdate(nextProps) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
+        hashHistory.push('/login');
+      }
+    }
 
-    this.state = { errors: [] };
-  }
-
-  componentWillUpdate(nextProps) {
-    // this.props // the old, current set of props
-    // nextProps // the next set of props that will be in place
-    // when the component rerenders
-    if (!this.props.data.user && nextProps.data.user) {
-      // redirect to dashboard!!!!
-      hashHistory.push('/dashboard');
+    render() {
+      return <WrappedComponent {...this.props} />;
     }
   }
 
-  onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query }]
-    }).catch(res => {
-      const errors = res.graphQLErrors.map(error => error.message);
-      this.setState({ errors });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Login</h3>
-        <AuthForm
-          errors={this.state.errors}
-          onSubmit={this.onSubmit.bind(this)}
-        />
-      </div>
-    );
-  }
-}
-
-export default graphql(query)(
-  graphql(mutation)(LoginForm)
-);
+  return graphql(currentUserQuery)(RequireAuth);
+};
